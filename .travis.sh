ff6992c2914c0ce89cc4b3e677e610c1d1e230be
MT <mtan.web+github@gmail.com>
2017-05-24 08:10:42 +0900

    Update travis task

--- .travis.sh
+++ .travis.sh
@@ -1,19 +1,46 @@
 #!/bin/bash
 THIS="${0##*/}"
 CDIR=$([ -n "${0%/*}" ] && cd "${0%/*}" 2>/dev/null; pwd)
+
 # Name
 THIS="${THIS:-.travis.yml}"
 BASE="${THIS%.*}"
+
+# Ansible playbook
+play=$(type -P ansible-playbook)
+
 # Errors
 errs=0
+
 # Run tests
 for test in \
 $(find "${CDIR}" -maxdepth 3 -type f -a -name "test.yml" 2>/dev/null)
 do
-  pushd "${test%/*}" && {
-  ansible-playbook -i inventory test.yml --syntax-check ||
+
+  [ -d "${test%/*}" ] &&
+  pushd "${test%/*}" ||
+  continue
+
+  [ -n "${test##*/}" ] && {
+
+    host=""
+
+    for file in inventory hosts
+    do 
+      [ ! -e "$file" ] || {
+        host="$file"; break
+      }
+    done || :
+
+    $play -i "${host}" "${test##*/}" --syntax-check &&
+    $play -i "${host}" "${test##*/}" -C
+
+  } ||
   errs=$((errs + 1))
-  popd || : }
+
+  popd || :
+
 done
-# end
+
+# End
 exit $errs
