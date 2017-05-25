c065e4baed8f86adaf6b1d607fb926bda557ac56
MT <mtan.web+github@gmail.com>
2017-05-26 08:53:06 +0900

    Update

--- .travis.sh
+++ .travis.sh
@@ -12,33 +12,52 @@ play=$(type -P ansible-playbook)
 # Errors
 errs=0
 
+# Run test func.
+test_syntaxcheck_after_run() {
+  local test="$1"
+  local host=""
+  local rval=1
+
+  [ -e "${test}" ]     || return 120
+  [ -d "${test%/*}" ]  || return 121
+  [ -n "${test##*/}" ] || return 122
+  pushd "${test%/*}"   || return 123
+
+  for file in inventory hosts
+  do 
+    [ ! -e "$file" ] || {
+    host="$file"; break; }
+  done || :
+
+  : && {
+
+    $play ${host:+-i $host} "${test##*/}" --syntax-check &&
+    $play ${host:+-i $host} "${test##*/}" -C
+
+  }
+  rval=$?
+
+  popd || :
+
+  return $rval
+}
+
+# OS spec
+test_syntaxcheck_after_run \
+"${CDIR}/os-spec/tests/test.yml" ||
+exit 1
+
 # Run tests
 for test in \
 $(find "${CDIR}" -maxdepth 3 -type f -a -name "test.yml" 2>/dev/null)
 do
-
-  [ -d "${test%/*}" ] &&
-  pushd "${test%/*}" ||
+  
+  echo "${test}" |
+  grep "/os-spec/tests/" 1>/dev/null 2>&1 ||
   continue
 
-  [ -n "${test##*/}" ] && {
-
-    host=""
-
-    for file in inventory hosts
-    do 
-      [ ! -e "$file" ] || {
-        host="$file"; break
-      }
-    done || :
-
-    $play -i "${host}" "${test##*/}" --syntax-check &&
-    $play -i "${host}" "${test##*/}" -C
-
-  } ||
+  test_syntaxcheck_after_run "${test}" ||
   errs=$((errs + 1))
-
-  popd || :
 
 done
 
